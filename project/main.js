2c3829135d235667a2ae5dbfe1847a947b3f4047
Abdul Mannan Khan <76775878+abdmankhan@users.noreply.github.com>
2024-04-03 04:16:02 +0530

    Update main.js

--- project/main.js
+++ project/main.js
@@ -1,6 +1,6 @@
 function redirecter1(){
-    window.location = "../QuizApp/html/index.html"
+    window.location = "QuizApp/html/index.html"
 }
 function redirecter2(){
-    window.location = "../TodoList/TodoList/project.html"
+    window.location = "TodoList/TodoList/project.html"
 }
